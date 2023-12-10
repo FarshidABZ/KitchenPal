f5cec50fd2b05113d9a14cf227280c1de1a4034e
Farshid Abazari <farshidabazari@gmail.com>
2023-12-10 17:30:10 +0100

    Fix runtime android test issue

--- core/common/src/androidTest/java/com/example/common/ExampleInstrumentedTest.kt
+++ core/common/src/androidTest/java/com/example/common/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.common.test", appContext.packageName)
+        assertEquals("com.core.common.test", appContext.packageName)
     }
 }
